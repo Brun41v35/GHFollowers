d77d62d17eb7f72a0005ed0a28c46dae0ee6123b
Bruno Alves <alves.bruno945@gmail.com>
2021-05-16 15:28:34 -0300

    Adding the modal textfield

---
+++ GitHub/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHub
+//
+//  Created by Bruno Silva on 16/05/21.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Functions
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemGroupedBackground
+        autocorrectionType          = .no
+        
+        placeholder                 = "Enter a username"
+    }
+}
